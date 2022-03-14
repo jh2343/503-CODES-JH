e79e3bd3d0d16f069c646c36625c3e55c682707b
jh6810 <jh6810@gmail.com>
2022-03-14 15:05:47 -0400

    SYNC-2022-03-14

---
+++ SHARE/WEEK-08/EX2-JS/app.js
@@ -0,0 +1,33 @@
+
+
+// RUN FROM COMMAND LINE --> node index.js
+// cntl+L --> clear consol
+
+
+// MODIFIED FROM 
+    // https://www.youtube.com/watch?v=W6NZfCO5SIk
+    // https://www.w3schools.com/js/js_loop_for.asp
+
+// DECLARE VARIABLES 
+// use meaningful names, no space/hypens, use camel case,
+// case sensitive, can't be reserved JS keywords 
+
+ 
+//------------------- 
+//DATA TYPES 
+//-------------------
+
+// PRIMITIVE TYPES
+// string,number,boolean, unidentified, null
+
+
+// OLD WAY
+ var MyVar1="test1";
+
+ //dynamic types
+ text=typeof MyVar1
+console.log(text);
+MyVar1=4
+console.log(typeof MyVar1);
+
+
